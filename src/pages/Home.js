cd59dfeb6a63666cda864ae5a1412805ccaffc1d
Prakhar <kriptyco@gmail.com>
2020-09-30 14:28:02 +0530

    Styled the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../styled/callToAction.js";
+import { Accent } from "../styled/Accents.js";
+import { StyledTitle } from "../styled/Title.js";
 
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <StyledTitle>Ready To Type ?</StyledTitle>
+            <CallToAction to="/game">
+                Click or Type <Accent>'S'</Accent> To Start Playing !
+            </CallToAction>
         </div>
-    )
+    );
 }
---
+++ src/styled/Title.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledTitle = styled.h1`
+    font-size: 3.5rem;
+    text-align: center;
+    margin-bottom: 3rem;
+    margin-top: 3rem;
+`;
