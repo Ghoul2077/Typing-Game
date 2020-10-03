6783552a52cae690fa78dee52fdf0adfaf01889b
Prakhar <kriptyco@gmail.com>
2020-10-04 01:40:54 +0530

    Switched to theme provider from styled components instead of plain css variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { ThemeProvider } from "styled-components";
 import Home from "./pages/Home.js";
 import GameOver from "./pages/GameOver.js";
 import Game from "./pages/Game.js";
@@ -9,27 +10,33 @@ import Navbar from "./components/Navbar.js";
 import { Container } from "./styled/Container.js";
 import { Main } from "./styled/Main.js";
 import Global from "./styled/Global.js";
+import { DarkTheme, LightTheme } from "./styled/Theme";
 
 function App() {
   const { loading } = useAuth0();
+  const [theme] = useState("light");
+
+  const currentTheme = theme === "light" ? LightTheme : DarkTheme;
 
   return (
     <Router>
-      <Global />
-      <Main>
-        {loading && <p>Loading....</p>}
-        {!loading && (
-          <Container>
-            <Navbar />
-            <Switch>
-              <Route path="/game" component={Game} />
-              <Route path="/highScores" component={HighScores} />
-              <Route path="/gameOver" component={GameOver} />
-              <Route path="/" component={Home} />
-            </Switch>
-          </Container>
-        )}
-      </Main>
+      <ThemeProvider theme={currentTheme}>
+        <Global />
+        <Main>
+          {loading && <p>Loading....</p>}
+          {!loading && (
+            <Container>
+              <Navbar />
+              <Switch>
+                <Route path="/game" component={Game} />
+                <Route path="/highScores" component={HighScores} />
+                <Route path="/gameOver" component={GameOver} />
+                <Route path="/" component={Home} />
+              </Switch>
+            </Container>
+          )}
+        </Main>
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ src/styled/Theme.js
@@ -0,0 +1,15 @@
+const sharedStyle = {
+    accent: "#e16365",
+};
+
+export const DarkTheme = {
+    mainBgColor: "#333",
+    mainTextColor: "#f9f9f9",
+    ...sharedStyle,
+};
+
+export const LightTheme = {
+    mainBgColor: "#f9f9f9",
+    mainTextColor: "#333",
+    ...sharedStyle,
+};
