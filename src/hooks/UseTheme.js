f2cc7913f5c7da63a84e1aee32c7ec7a76b5186b
Prakhar <kriptyco@gmail.com>
2020-10-04 02:04:54 +0530

    Added persisting theme preference in localstorage

--- src/hooks/UseTheme.js
+++ src/hooks/UseTheme.js
@@ -3,8 +3,17 @@ import { useState, useEffect } from "react";
 export default () => {
     const [theme, setTheme] = useState("light");
 
+    useEffect(() => {
+        const localStorageTheme = window.localStorage.getItem("theme");
+        if (localStorageTheme) {
+            setTheme(localStorageTheme);
+        }
+    }, []);
+
     function toggleTheme() {
-        setTheme(theme === "light" ? "dark" : "light");
+        const newTheme = theme === "light" ? "dark" : "light";
+        window.localStorage.setItem("theme", newTheme);
+        setTheme(newTheme);
     }
 
     return [theme, toggleTheme];
